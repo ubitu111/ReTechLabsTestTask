9878564afdd1a66fa3c1fa510d164831ac43c446
Vladimir Kireev <ubitu111@yandex.ru>
2021-03-26 23:01:53 +0700

    Организация структуры пакетов, автоизменения студии

---
+++ app/src/main/java/ru/kireev/retechlabstesttask/view/MainActivity.kt
@@ -0,0 +1,32 @@
+package ru.kireev.retechlabstesttask.view
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import ru.kireev.retechlabstesttask.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.refresh_menu) {
+            Toast.makeText(this, "REFRESH", Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
